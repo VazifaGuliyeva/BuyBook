54cce3a24fcb917081d0fc678183df990627731d
VazifaGuliyeva <vezife07102002@gmail.com>
2023-07-29 21:24:31 +0400

    BuyBook/form based authentication

--- src/main/java/com/example/buybook/config/SecurityConfig.java
+++ src/main/java/com/example/buybook/config/SecurityConfig.java
@@ -8,17 +8,41 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails userDetails1  = User
+                .withUsername("vazifa")
+                .password(passwordEncoder().encode("vazifa"))
+                .roles("USER")
+                .build();
+
+        UserDetails userDetails2  = User
+                .withUsername("laman")
+                .password(passwordEncoder().encode("laman"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails1,userDetails2);
+    }
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http
                     .csrf()
-
                     .disable();
             http.authorizeHttpRequests().anyRequest().authenticated();
             http.formLogin().permitAll();
